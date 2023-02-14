0e03e8ce1f933574a610b5cda6916e13c9ba8494
IrishAir <bls21@mail.ru>
2023-02-14 15:10:18 +0500

    form footer

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -10,8 +10,24 @@ function Form() {
 					initialValue={3300000}
 					variant='price'
 				/>
+				<Input
+					label='Первоначальный взнос'
+					initialValue={420000}
+					variant='fee'
+				/>
+				<Input label='Срок лизинга' initialValue={60} variant='month' />
 			</fieldset>
-			<footer className={styles.form__footer}></footer>
+			<footer className={styles.form__footer}>
+				<div className={styles.form__details}>
+					<p>Сумма договора лизинга</p>
+					<p className='text--large'>4 467 313 ₽</p>
+				</div>
+				<div className={styles.form__details}>
+					<p>Ежемесячный платеж от</p>
+					<p className='text--large'>114 455 ₽</p>
+				</div>
+        <button>Отправить</button>
+			</footer>
 		</form>
 	)
 }
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -3,35 +3,33 @@ import { formatNumber } from '@utils/formatNumbers'
 import styles from './Input.module.scss'
 import classnames from 'classnames'
 
-function Input({ label, initialValue, variant }) {
+function Input({ label, initialValue, variant}) {
 	const [value, setValue] = useState(initialValue)
 	return (
-		<div className={styles.input}>
-			<label>
-				<p className={styles.input__label}>{label}</p>
-				<div className={styles.input__fields}>
-					<span className={styles.input__wrapper}>
-						<input
-							value={formatNumber(value)}
-							onChange={e => setValue(e.target.value)}
-							type='text'
-							className={classnames(styles.input__field, styles.input__number)}
-						/>
-						<p className={styles.input__suffix}>
-							{variant == 'price' ? '₽' : variant == 'month' ? 'мес.' : ''}
-						</p>
-					</span>
+		<label className={styles.input}>
+			<p className={styles.input__label}>{label}</p>
+			<div className={styles.input__fields}>
+				<span className={styles.input__wrapper}>
 					<input
-						type='range'
-						min={variant == 'price' ? 1500000 : variant == 'month' ? 6 : 10}
-						max={variant == 'price' ? 10000000 : variant == 'month' ? 120 : 60}
-						value={value}
-						onChange={e => setValue(formatNumber(e.target.value))}
-						className={classnames(styles.input__field, styles.input__range)}
+						value={formatNumber(value)}
+						onChange={e => setValue(e.target.value)}
+						type='text'
+						className={classnames(styles.input__field, styles.input__number)}
 					/>
-				</div>
-			</label>
-		</div>
+					<p className={styles.input__suffix}>
+						{variant == 'price' ? '₽' : variant == 'month' ? 'мес.' : ''}
+					</p>
+				</span>
+				<input
+					type='range'
+					min={variant == 'price' ? 1500000 : variant == 'month' ? 6 : 10}
+					max={variant == 'price' ? 10000000 : variant == 'month' ? 120 : 60}
+					value={value}
+					onChange={e => setValue(formatNumber(e.target.value))}
+					className={classnames(styles.input__field, styles.input__range)}
+				/>
+			</div>
+		</label>
 	)
 }
 
