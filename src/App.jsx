817d6269782c6fe5e644f8bf4b4c89fee22c850c
IrishAir <bls21@mail.ru>
2023-02-14 08:50:37 +0500

    added breakpoints and typography

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 function App() {
-	return <main className='App'>App</main>
+	return (
+		<main className='App'>
+			<h1>Заголовок</h1>
+			<p>Текст</p>
+			<p className='text text--bold'>Текст</p>
+			<p className='text text--large'>Текст</p>
+		</main>
+	)
 }
 
 export default App
